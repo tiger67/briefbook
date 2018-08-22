c931fe081390081f30a6f484a77c7e4af150552e
kuangliting <105418767@qq.com>
2018-08-22 09:22:29 +0800

    ”
    add index nav icon

--- src/views/nav/Index.vue
+++ src/views/nav/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav nav-fixed nav-wrap">
-    <div class="nav-layout">
+    <div class="nav-layout" style="position:relative">
       <router-link class="logo" to="/"><img src="../../assets/nav-logo.png"></router-link>
       <router-link class="btn write-btn" to="/writer">
         <i class="iconfont ic-write"></i>写文章
@@ -8,7 +8,7 @@
       <router-link to="/404" class="style-mode-btn">
         <i class="iconfont ic-navigation-mode"></i>
       </router-link>
-      <div style="position:fixed;right:250px;top:20px;font-size:16px;">
+      <div style="position:absolute;right:220px;top:20px;font-size:16px;">
         <router-link to="/sign_in">登录</router-link>
         <router-link to="/sign_up">注册</router-link>
         <a href="javascript:void(0);" @click.prevent="toggle(true)">登录框</a>
@@ -23,16 +23,18 @@
           <ul class="nav">
             <li class="nav-tab nav">
               <router-link to="/">
-                <span class="menu-text">发现</span>
+                <i class="iconfont ic-navigation-discover menu-icon"></i><span class="menu-text">发现</span>
               </router-link>
             </li>
             <li class="nav-tab">
               <router-link to="/follow">
+                <i class="iconfont ic-navigation-follow menu-icon"></i>
                 <span class="menu-text">关注</span>
               </router-link>
             </li>
             <li class="nav-tab notification">
               <router-link to="/news" class="notification-btn">
+                <i class="iconfont ic-navigation-notification menu-icon"></i>
                 <span class="menu-text">消息</span>
                 <span class="badge">1</span>
                 <sub-nav class='sub-nav'></sub-nav>
@@ -87,124 +89,131 @@ export default {
 </script>
 <style>
 .sub-nav {
-    display: none;
-    z-index: 3;
-    background-color: #fff;
+  display: none;
+  z-index: 3;
+  background-color: #fff;
 }
 
 .nav-tab:hover .sub-nav {
-    display: block
+  display: block
 }
 
 .nav-fixed {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    border-style: solid;
-    border-width: 0 0 1px;
-    border-color: #e7e7e7;
-    background: #fff;
-    z-index: 999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  border-style: solid;
+  border-width: 0 0 1px;
+  border-color: #e7e7e7;
+  background: #fff;
+  z-index: 999;
 }
 
 .nav {
-    height: 56px;
+  height: 56px;
 }
 
 .menu-text {
-    font-size: 17px;
+  font-size: 17px;
+}
+
+.menu-icon {
+  display: inherit;
+  float: left;
+  font-size: 20px;
+  margin-right: 5px;
 }
 
 .nav-wrap {
-    margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 
 .nav .style-mode-btn {
-    float: right;
-    line-height: 20px;
-    padding: 17px 10px 17px 15px;
-    font-size: 24px;
-    color: #969696;
+  float: right;
+  line-height: 20px;
+  padding: 17px 10px 17px 15px;
+  font-size: 24px;
+  color: #969696;
 }
 
 .nav .write-btn {
-    float: right;
-    width: 100px;
-    height: 40px;
-    line-height: 24px;
-    margin: 8px 15px 0;
-    border-radius: 20px;
-    font-size: 15px;
-    color: #fff;
-    background-color: #ea6f5a;
+  float: right;
+  width: 100px;
+  height: 40px;
+  line-height: 24px;
+  margin: 8px 15px 0;
+  border-radius: 20px;
+  font-size: 15px;
+  color: #fff;
+  background-color: #ea6f5a;
 }
 
 .write-btn .ic-write {
-    margin-right: 3px;
-    font-size: 19px;
-    vertical-align: middle;
+  margin-right: 3px;
+  font-size: 19px;
+  vertical-align: middle;
 }
 
 .badge {
-    padding: 3px 6px;
-    font-size: 13px!important;
-    background-color: #ea6f5a;
+  padding: 3px 6px;
+  font-size: 13px!important;
+  background-color: #ea6f5a;
 }
 
 .nav-list {
-    margin-left: -15px;
+  margin-left: -15px;
 }
 
 .nav-layout {
-    min-width: 768px;
-    max-width: 1440px;
-    margin: 0 auto;
-    height: 100%;
+  min-width: 768px;
+  max-width: 1440px;
+  margin: 0 auto;
+  height: 100%;
 }
 
 .nav-menu-collapse {
-    margin-left: -15px;
+  margin-left: -15px;
 }
 
 .logo img {
-    height: 100%;
+  height: 100%;
 }
 
 .nav-tab {
-    float: left;
-    margin-right: 10px;
-    position: relative;
+  float: left;
+  margin-right: 10px;
+  position: relative;
 }
 
 .nav-tab>a {
-    display: block;
-    height: 56px;
-    line-height: 26px;
-    padding: 15px;
-    text-decoration: none;
-    position: relative;
+  display: block;
+  height: 56px;
+  line-height: 26px;
+  padding: 15px;
+  text-decoration: none;
+  position: relative;
 }
 
 .nav-tab>.router-link-exact-active {
-    color: #ea6f5a;
+  color: #ea6f5a;
 }
 
 .nav-tab>a:not(.router-link-exact-active):hover {
-    background-color: #f5f5f5
+  background-color: #f5f5f5
 }
 
 .nav-layout>.logo {
-    float: left;
-    height: 56px;
-    padding: 0;
+  float: left;
+  height: 56px;
+  padding: 0;
 }
 
 .notification-btn .badge {
-    position: absolute;
-    top: 10px;
-    right: -5px;
-    color: #fff!important;
+  position: absolute;
+  top: 10px;
+  right: -5px;
+  color: #fff!important;
 }
 
 </style>
